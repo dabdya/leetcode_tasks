a22f274fe6fdaf702c6dcb93ad66ed9eb61b700b
Artem Konovalov <58086532+dabdya@users.noreply.github.com>
2021-09-01 23:40:20 +0500

    Delete 645.py

--- 645.py
+++
@@ -1,5 +0,0 @@
-class Solution:
-    def findErrorNums(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            if i+1 != nums[i]:
-                return [nums[i], i+1]
