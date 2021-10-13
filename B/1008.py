5cae802f2a5891da2351824c780e343dd62c988e
Artem Konovalov <58086532+dabdya@users.noreply.github.com>
2021-10-13 13:00:20 +0500

    B 1008

---
+++ B/1008.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        
+        root = TreeNode(preorder[0])
+        
+        stack = [root]
+        
+        for i in range(1, len(preorder)):
+            node = TreeNode(preorder[i])
+            if node.val < stack[-1].val:
+                stack[-1].left = node
+            else:
+            
+                while len(stack) != 1:
+                    if node.val < stack[-1].val:
+                        break
+                    pop = stack.pop()
+
+                if node.val > stack[-1].val:
+                    stack[-1].right = node
+                    stack.pop()
+                else:
+                    pop.right = node 
+                    
+            stack.append(node)
+                        
+        return root
